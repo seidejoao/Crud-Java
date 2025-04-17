64440262757db008ad55a846ec2a6fbf0d15ad12
João Victor Seide <seidejvs@gmail.com>
2025-04-17 00:39:03 -0300

    criado o modelo/objeto usado para o bd

---
+++ Crud/src/object/Object.java
@@ -0,0 +1,27 @@
+package object;
+
+public class Object {
+
+    private String nome;
+    private String autor;
+    private String genero;
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getAutor() {
+        return autor;
+    }
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+    public String getGenero() {
+        return genero;
+    }
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+}
